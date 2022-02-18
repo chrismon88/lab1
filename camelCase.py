113fc9f9b476c665df11750bec98f2ad62399467
Your Name <you@example.com>
2022-02-18 16:09:21 -0600

    added test

--- camelCase.py
+++ camelCase.py
@@ -1,16 +1,20 @@
 #create function for parsing user inptu
 def parser(userInput):
-    string_list = userInput.split()#splits string into individual elements in list
+    try:
+        string_list = userInput.split()#splits string into individual elements in list
 
-    new_list = [] # variable creating empty list used to store elements
-    for e in string_list:
-        new_list.append((e.title())) #capatalizing first letter of elements in list
+        new_list = [] # variable creating empty list used to store elements
+        for e in string_list:
+            new_list.append((e.title())) #capatalizing first letter of elements in list
 
-    first_word = new_list[0].lower()
-    new_list[0] = first_word
+        first_word = new_list[0].lower()
+        new_list[0] = first_word
 
-    j = "".join(new_list)
-    return j
+        j = "".join(new_list)
+        return j
+    except:
+        print("The input you entered is invalid.")
+        return userInput
 
 def banner():
     """ Display program name """
